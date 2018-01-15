49059a27364ac5f1b6da3e516d6ef12123bc29a5
rscsam <sam.costley@yahoo.com>
2018-01-15 14:54:04 -0500

    Changed to test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+
 app.get('/test/', (req, res) => {
   res.send('Server reached successfully')
-})
+});
+app.get('', (req, res) => {
+	res.send('Default landing page')
+});
 app.listen(3000, () => console.log('Server running on port 3000'))
