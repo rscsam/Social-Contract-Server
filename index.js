3b2c66e5de576b6dbcef6d67de96bb4bdd1751d0
EC2 Default User <ec2-user@ip-172-31-26-32.us-east-2.compute.internal>
2018-01-15 19:42:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+app.get('/test/', (req, res) => {
+  res.send('Server reached successfully')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
